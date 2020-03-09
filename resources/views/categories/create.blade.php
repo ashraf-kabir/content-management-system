bed6c868d52e22cf08e48814a809f5dfe08556b1
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-03-09 21:17:02 +0600

    categories updated

---
+++ resources/views/categories/create.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="card card-default">
+        <div class="card-header">Create Category</div>
+        <div class="card-body">
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul class="list-group">
+                        @foreach ($errors->all() as $error)
+                            <li class="list-group-item text-danger">
+                                {{ $error }}
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            <form action="{{ route('categories.store') }}" method="POST">
+                @csrf
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" id="name" class="form-control" name="name">
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-success">Add Category</button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="d-flex justify-content-end mb-2">
+        <a href="{{ route('categories.create') }}" class="btn btn-success">Add Categories</a>
+    </div>
+    <div class="card card-default">
+        <div class="card-header">Categories</div>
+        <div class="card-body">
+            <table class="table">
+                <thead>
+                    <th>Name</th>
+                </thead>
+                <tbody>
+                    @foreach ($categories as $category)
+                        <tr>
+                            <td>
+                                {{ $category->name }}
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
