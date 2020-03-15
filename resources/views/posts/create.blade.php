eeefd0df46e8d11ffb76d937fa157305a8d062fe
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-03-15 23:00:55 +0600

    updating post implemented

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,31 +2,40 @@
 
 @section('content')
     <div class="card card-default">
-        <div class="card-header">Create Post</div>
+        <div class="card-header">{{ isset($post) ? 'Edit Post': 'Create Post' }}</div>
         <div class="card-body">
-            <form action="{{ route('posts.store') }}" method="POST" enctype="multipart/form-data">
+            <form action="{{ isset($post) ? route('posts.update', $post->id) : route('posts.store') }}" method="POST" enctype="multipart/form-data">
                 @csrf
+
+                @if (isset($post))
+                    @method('PUT')
+                @endif
                 <div class="form-group">
                     <label for="title">Title</label>
-                    <input type="text" class="form-control" name="title" id="title">
+                    <input type="text" class="form-control" name="title" id="title" value="{{ isset($post) ? $post->title : '' }}">
                 </div>
 
                 <div class="form-group">
                     <label for="description">Description</label>
-                    <textarea name="description" id="description" cols="5" rows="5" class="form-control"></textarea>
+                    <textarea name="description" id="description" cols="5" rows="5" class="form-control">{{ isset($post) ? $post->description : '' }}</textarea>
                 </div>
 
                 <div class="form-group">
                     <label for="content">Content</label>
-                    <input id="content" type="hidden" name="content">
+                    <input id="content" type="hidden" name="content" value="{{ isset($post) ? $post->content : '' }}">
                     <trix-editor input="content"></trix-editor>
                 </div>
 
                 <div class="form-group">
                     <label for="published_at">Published At</label>
-                    <input type="text" class="form-control" name="published_at" id="published_at">
+                    <input type="text" class="form-control" name="published_at" id="published_at" value="{{ isset($post) ? $post->published_at : '' }}">
                 </div>
 
+                @if (isset($post))
+                    <div class="form-group">
+                        <img src="{{ asset('storage/'.$post->image) }}" width="100%">
+                    </div>
+                @endif
                 <div class="form-group">
                     <label for="image">Image</label>
                     <input type="file" class="form-control" name="image" id="image">
@@ -34,7 +43,7 @@
 
                 <div class="form-group">
                     <button type="submit" class="btn btn-success">
-                        Create Post
+                        {{ isset($post) ? 'Update post' : 'Create post'}}
                     </button>
                 </div>
             </form>
