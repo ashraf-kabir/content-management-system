8c742bb29cf78855a2a7ada3b23f49a53cda05f5
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-03-16 16:11:07 +0600

    Update create.blade.php

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -62,7 +62,7 @@
                 @if ($tags->count() > 0)
                 <div class="form-group">
                     <label for="tags">Tags</label>
-                    <select name="tags[]" id="tags" class="form-control" multiple>
+                    <select name="tags[]" id="tags" class="form-control tags-selector" multiple>
                         @foreach ($tags as $tag)
                             <option value="{{ $tag->id }}"
                                 @if (isset($post))
@@ -91,9 +91,14 @@
 @section('scripts')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.2.1/trix.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js"></script>
     <script>
         flatpickr('#published_at', {
             enableTime: true
+        });
+
+        $(document).ready(function() {
+            $('.tags-selector').select2();
         });
     </script>
 @endsection
@@ -101,4 +106,5 @@
 @section('css')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/trix/1.2.1/trix.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" rel="stylesheet" />
 @endsection
