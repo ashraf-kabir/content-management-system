a08163afb7b4f9ae3245800bae708051c783e92c
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-03-16 11:31:38 +0600

    middleware updated

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -10,6 +10,11 @@ use App\Category;
 
 class PostsController extends Controller
 {
+
+    public function __construct() {
+        $this->middleware('verifyCategoriesCount')->only(['create', 'store']);
+    }
+
     /**
      * Display a listing of the resource.
      *
