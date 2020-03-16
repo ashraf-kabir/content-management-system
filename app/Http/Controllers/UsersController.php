e2d1edb0b51b52f765726ea405c5149a64e687a3
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-03-16 19:50:46 +0600

    edit profile added

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,10 +5,29 @@ namespace App\Http\Controllers;
 use App\User;
 use Illuminate\Http\Request;
 
+use App\Http\Requests\Users\UpdateProfileRequest;
+
 class UsersController extends Controller
 {
     public function index() {
         return view('users.index')->with('users', User::all());
+    }
+
+    public function update(UpdateProfileRequest $request) {
+        $user = auth()->user();
+
+        $user->update([
+            'name' => $request->name,
+            'about' => $request->about
+        ]);
+
+        session()->flash('success', 'User updated successfully.');
+
+        return redirect()->back();
+    }
+
+    public function edit() {
+        return view('users.edit')->with('user', auth()->user());
     }
 
     public function makeAdmin(User $user) {
--- app/User.php
+++ app/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+
 class User extends Authenticatable
 {
     use Notifiable;
@@ -16,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'about',
     ];
 
     /**
