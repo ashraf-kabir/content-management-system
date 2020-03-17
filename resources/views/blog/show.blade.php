18a4668e5459f1689ad3357dec4c96c126da4e8a
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-03-17 13:37:20 +0600

    Update show.blade.php

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -47,7 +47,7 @@
 
                 {!! $post->content !!}
 
-                
+                <br>
                 <!-- Go to www.addthis.com/dashboard to customize your tools -->
                 <div class="addthis_inline_share_toolbox"></div>
             
