a15c67e6335e3697d0b28165e37b5638c73001f7
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-03-17 12:16:09 +0600

    search funtionality implemented

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -10,9 +10,17 @@ use Illuminate\Http\Request;
 class WelcomeController extends Controller
 {
     public function index() {
+
+        $search = request()->query('search');
+        if ($search) {
+            $posts = Post::where('title','LIKE', "%{$search}%")->simplepaginate(1);
+        } else {
+            $posts = Post::simplepaginate(4);
+        }
+
         return view('welcome')
         ->with('categories', Category::all())
         ->with('tags', Tag::all())
-        ->with('posts', Post::simplepaginate(4));
+        ->with('posts', $posts);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,7 +34,7 @@
           <div class="col-md-8 col-xl-9">
             <div class="row gap-y">
 
-              @foreach ($posts as $post)
+              @forelse ($posts as $post)
                 <div class="col-md-6">
                   <div class="card border hover-shadow-6 mb-6 d-block">
                     <a href="{{ route('blog.show', $post->id) }}"><img class="card-img-top" src="{{ asset('storage/'.$post->image) }}" alt="Card image cap"></a>
@@ -48,16 +48,16 @@
                     </div>
                   </div>
                 </div>
-              @endforeach
+              @empty
+                <p class="text center">
+                  No results found for query <strong>{{ request()->query('search') }}</strong>
+                </p>
+              @endforelse
               
             </div>
 
 
-            {{-- <nav class="flexbox mt-30">
-              <a class="btn btn-white disabled"><i class="ti-arrow-left fs-9 mr-4"></i> Newer</a>
-              <a class="btn btn-white" href="#">Older <i class="ti-arrow-right fs-9 ml-4"></i></a>
-            </nav> --}}
-            {{ $posts->links() }}
+            {{ $posts->appends(['search' => request()->query('search') ])->links() }}
           </div>
 
 
@@ -66,8 +66,8 @@
             <div class="sidebar px-4 py-md-0">
 
               <h6 class="sidebar-title">Search</h6>
-              <form class="input-group" target="#" method="GET">
-                <input type="text" class="form-control" name="s" placeholder="Search">
+              <form class="input-group" action="{{ route('welcome') }}" method="GET">
+                <input type="text" class="form-control" name="search" placeholder="Search" value="{{ request()->query('search') }}">
                 <div class="input-group-addon">
                   <span class="input-group-text"><i class="ti-search"></i></span>
                 </div>
