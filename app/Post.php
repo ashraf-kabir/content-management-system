612628687b1dac40c35d901dc813cdf8491d6905
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-03-17 13:33:52 +0600

    published_at fixed

--- app/Post.php
+++ app/Post.php
@@ -9,6 +9,10 @@ class Post extends Model
 {
 
     use SoftDeletes;
+
+    protected $dates = [
+        'published_at'
+    ];
     
     protected $fillable = [
         'title', 'description', 'content', 'image', 'published_at', 'category_id', 'user_id'
@@ -44,13 +48,17 @@ class Post extends Model
         return $this->belongsTo(User::class);
     }
 
+    public function scopePublished($query) {
+        return $query->where('published_at', '<=', now());
+    }
+
     public function scopeSearched($query) {
         $search = request()->query('search');
 
         if (!$search) {
-            return $query;
+            return $query->published();
         }
 
-        return $query->where('title', 'LIKE', "%{$search}%");
+        return $query->published()->where('title', 'LIKE', "%{$search}%");
     }
 }
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -94,7 +94,8 @@
     <script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js"></script>
     <script>
         flatpickr('#published_at', {
-            enableTime: true
+            enableTime: true,
+            enableSeconds: true
         });
 
         $(document).ready(function() {
